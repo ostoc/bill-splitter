de224c9686192d93151e062c64fa81bc7b191594
Junyu Pu <jp@3yourmind.com>
2019-02-25 17:38:21 +0100

    feature: add expense record

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
   <div id="app">
+    <h1>Expense Record</h1>
+    <div>
+      <div v-for="(sharer, index) in sharers" :key="index">
+        <button @click="addSharer(sharer.name)">{{ sharer.name }}</button>
+      </div>
+      {{ selectedSharer }}
+      <label>Amount</label>
+      <input type="number" v-model="amount" />
+      <button @click="addRecord">Add</button>
+    </div>
+
+    {{ indivalBook }}
+
+    <ul>
+      <li v-for="(record, index) in expensRecords" :key="index">
+        {{ record.names }} {{ record.amount }}
+        <button @click="removeRecord(record)">Remove</button>
+      </li>
+    </ul>
+    {{ expensRecords }}
+    <H2>Payed</H2>
+    <select v-model="payer">
+      <option
+        v-for="(sharer, index) in sharers"
+        :key="index"
+        :value="sharer.name"
+        >{{ sharer.name }}</option
+      >
+    </select>
+    <input v-model="paidAmount" />
+    <button @click="addPayRecord">Add</button>
+    <ul>
+      <li v-for="(paid, index) in paidRecods" :key="index">
+        {{ paid.name }} - {{ paid.amount }}
+      </li>
+    </ul>
+
     <ul>
       <li v-for="(trans, index) in transferBook" :key="index">
         {{ trans.to }} {{ trans.amount }}
       </li>
     </ul>
-    <ul>
-      <li v-for="(record, index) in balanceBook" :key="index">
-        {{ record.name }} <input v-model="record.balance" />
-      </li>
-    </ul>
-    {{ balanceBook }}
+    {{ newBalance }}
+    <!-- {{ balanceFinnal }} -->
     <button @click="cals">Cal</button>
   </div>
 </template>
@@ -20,28 +53,79 @@ export default {
   name: "app",
   data() {
     return {
+      amount: null,
+      selectedSharer: [],
       transferBook: [],
+      recordTitle: null,
+      payer: null,
+      paidAmount: null,
+      sharers: [
+        { name: "Alice", balance: 0 },
+        { name: "Bob", balance: 0 },
+        { name: "Caro", balance: 0 },
+        { name: "David", balance: 0 }
+      ],
+      expensRecords: [],
+      paidRecods: [],
       balanceBook: [
         {
           name: "Alice",
-          balance: 12
+          balance: 0
         },
         {
           name: "Bob",
-          balance: -21
+          balance: 0
         },
         {
           name: "Caro",
-          balance: 21
+          balance: 0
         },
         {
           name: "David",
-          balance: -12
+          balance: 0
         }
       ]
     };
   },
+  computed: {
+    newBalance() {
+      this.sharers.forEach(share => {
+        this.expensRecords.forEach(record => {
+          if (record.names.includes(share.name)) {
+            const sharedAmount = record.amount / record.names.length;
+            share.balance = share.balance + sharedAmount;
+          }
+        });
+      });
+      return this.sharers;
+    }
+  },
   methods: {
+    addRecord() {
+      const record = {
+        names: this.selectedSharer,
+        amount: this.amount
+      };
+      this.expensRecords.push(Object.create(record));
+    },
+    removeRecord(record) {
+      this.expensRecords.splice(this.expensRecords.indexOf(record), 1);
+    },
+    addPayRecord() {
+      this.paidRecods.push({
+        name: this.payer,
+        amount: this.paidAmount
+      });
+    },
+    addSharer(sharer) {
+      if (this.selectedSharer.includes(sharer)) {
+        this.selectedSharer = this.selectedSharer.filter(
+          selected => selected !== sharer
+        );
+        return this.selectedSharer;
+      }
+      this.selectedSharer.push(sharer);
+    },
     cals() {
       const dynamicBalanceBook = this.balanceBook.map(a =>
         Object.assign([], a)
