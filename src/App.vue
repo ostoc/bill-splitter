9f685fefb3088018fd68a9f807fc765cb8182a7b
Junyu Pu <jp@3yourmind.com>
2019-03-29 11:13:40 +0100

    style: change styles
    Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
     <h2>Sharers</h2>
-    <div class="row sharers">
-      <div class="sharer" v-for="(sharer, index) in sharers" :key="index">
-        <span v-text="sharer" />
-        <span @click="removeSharer(sharer)" class="sharer__delete-button"
-          >X</span
-        >
+    <div class=" sharers">
+      <div class="row">
+        <div class=" sharer" v-for="(sharer, index) in sharers" :key="index">
+          <span v-text="sharer" />
+          <span @click="removeSharer(sharer)" class="sharer__delete-button"
+            >✖︎</span
+          >
+        </div>
+      </div>
+      <div class="row">
+        <input v-model="newSharer" style="flex:2 1" />
+        <button @click="addSharer()" style="flex:1 1">ADD</button>
       </div>
     </div>
-    <div class="row">
-      <input v-model="newSharer" />
-      <button @click="addSharer()">ADD</button>
-    </div>
+
     <h2>Expense Records</h2>
     <div class="expense">
-      <div class="row">
-        <input v-model="recordTitle" placeholder="Title" />
+      <div>
+        <div class="row">
+          <input v-model="recordTitle" placeholder="Title" style="flex: 1 1" />
+        </div>
         <input type="number" v-model="amount" placeholder="Amount" />
         <button @click="addRecord">Add</button>
       </div>
@@ -37,14 +42,6 @@
       </div>
     </div>
     <ExpanseTable :tableData="expenseRecords" />
-    <!-- <div class="expense-list">
-      <ul>
-        <li v-for="(record, index) in expenseRecords" :key="index">
-          {{ record.names }} {{ record.amount }}
-          <button @click="removeRecord(expenseRecords, record)">Remove</button>
-        </li>
-      </ul>
-    </div> -->
 
     <IndividualExpanseTable :data="individualExpanse" />
 
@@ -227,89 +224,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  font-size: 16px;
-  max-width: 480px;
-  margin: 0 auto;
-}
-.expense {
-  background: #ddd;
-  border: 1px solid #888;
-  display: flex;
-  padding: 8px;
-  display: flex;
-  flex-direction: column;
-}
-.row {
-  display: flex;
-  margin: 8px 0;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-.sharers {
-  /* background: #ddd; */
-}
-.sharer {
-  border-radius: 8px;
-  padding: 8px;
-  line-height: 20px;
-  display: flex;
-  align-content: center;
-  width: auto;
-  border: 1px solid #ddd;
-  margin-right: 8px;
-}
-.sharer .sharer__delete-button {
-  background: #d8255b;
-  border-radius: 100%;
-  height: 20px;
-  margin-left: 8px;
-  text-align: center;
-  color: #fff;
-  width: 20px;
-}
-.sharer .sharer__delete-button:hover {
-  cursor: pointer;
-  background: #bb1144;
-}
-.sharer-selector {
-  height: 40px;
-  width: 40px;
-  display: flex;
-  background: #999;
-  border-radius: 100%;
-  border: 1px solid #999;
-  align-items: center;
-  text-align: center;
-  color: #fff;
-  text-transform: uppercase;
-}
-.sharer-selector:hover {
-  cursor: pointer;
-}
-.sharer-selector--selected {
-  background: green;
-}
-button {
-  background: #3a740a;
-  height: 40px;
-  border: 0;
-  border-radius: 4px;
-  color: #fff;
-  min-width: 80px;
-}
-input {
-  height: 36px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  font-size: 16px;
-  padding-left: 4px;
-}
-select {
-  height: 40px;
-  font-size: 16px;
-}
-</style>
